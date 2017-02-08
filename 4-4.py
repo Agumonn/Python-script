767e6bb58adb11dae41ef4003eef13d9b324b63d
Agumonn <K8522@student.jamk.fi>
2017-02-08 12:26:42 +0200

    Update 4-4.py

--- 4-4.py
+++ 4-4.py
@@ -14,13 +14,18 @@ country
 
 dicteng = {"LANGUAGE":"KIELI", "DOG": "KOIRA", "CAT":"KISSA", "CITY": "KAUPUNKI", "COUNTRY":"MAA", "AIRPLANE":"LENTOKONE"}
 dictfin = {"KIELI":"LANGUAGE", "KOIRA": "DOG", "KISSA":"CAT", "KAUPUNKI": "CITY", "MAA":"COUNTRY", "LENTOKONE":"AIRPLANE"} 
-dictionaries = {"dicteng" : dicteng, "dictfin" : dictfin}
 while True:
+   lippu=0
    string=input("anna sana Give a word: ")
    stringiso = string.upper()
    for match in dicteng:
       if stringiso==match:
          print(dicteng[stringiso])
+         lippu+=1
    for match in dictfin:
       if stringiso==match:
          print(dictfin[stringiso])
+         lippu+=1
+   if lippu==0:
+      print("No match found")
+   
